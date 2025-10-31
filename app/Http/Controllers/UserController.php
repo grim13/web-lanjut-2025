958636985952fbc1a0c5eb016d157f344502febe
muhammad bunyamin <muhammad.bunyamin@radenfatah.ac.id>
2025-10-31 15:00:12 +0700

    controller user with data collection

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,11 +3,22 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\User;
 
 class UserController extends Controller
 {
     public function allUsers()
     {
-        return "All Users";
+        $users = 
+            User::select([
+                    'users.id', 
+                    'users.name', 
+                    'email', 
+                    'role_id', 
+                    'roles.name as role_name'
+                ])
+                ->join('roles', 'users.role_id', '=', 'roles.id')
+                ->get();
+        return $users;
     }
 }
